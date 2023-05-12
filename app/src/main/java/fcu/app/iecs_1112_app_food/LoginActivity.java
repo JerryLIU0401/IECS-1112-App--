2898a150e3a780281ad054ce21bf055dfd444bd2
JerryLIU <jerry2003.0401@gmail.com>
2023-05-12 18:09:42 +0800

    login & register page

--- app/src/main/java/fcu/app/iecs_1112_app_food/LoginActivity.java
+++ app/src/main/java/fcu/app/iecs_1112_app_food/LoginActivity.java
@@ -2,13 +2,31 @@ package fcu.app.iecs_1112_app_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        register_btn = findViewById(R.id.register_btn_login);
+
+        View.OnClickListener onClickRegisterBtnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        register_btn.setOnClickListener(onClickRegisterBtnListener);
     }
+
+
 }
---
+++ app/src/main/java/fcu/app/iecs_1112_app_food/RegisterActivity.java
@@ -0,0 +1,16 @@
+package fcu.app.iecs_1112_app_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+
+    }
+}

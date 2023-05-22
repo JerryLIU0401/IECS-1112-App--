97af0696f4b88f27f4a230d8246772f5c6ef71df
nguyenthanhphuoc <thanhphuoc1306vn@gmail.com>
2023-05-22 17:36:22 +0800

    edit profile activity, edit password activity, forgot password activity were done

--- app/src/main/java/fcu/app/iecs_1112_app_food/SettingActivity.java
+++ app/src/main/java/fcu/app/iecs_1112_app_food/SettingActivity.java
@@ -2,9 +2,30 @@ package fcu.app.iecs_1112_app_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SettingActivity extends PageBarButton {
+
+
+    @SuppressLint("WrongViewCast")
+    private Button btnEditProfile = findViewById(R.id.edit_profile_btn);
+    @SuppressLint("WrongViewCast")
+    private Button btnChangePass = findViewById(R.id.change_password_btn);
+    @SuppressLint("WrongViewCast")
+    private Button btnPaymentMethod = findViewById(R.id.payment_method_btn);
+    @SuppressLint("WrongViewCast")
+    private TextView tvEditProfile = findViewById(R.id.tv_edit_profile);
+    @SuppressLint("WrongViewCast")
+    private TextView tvChangePass = findViewById(R.id.tv_change_password);
+    @SuppressLint("WrongViewCast")
+    private TextView tvPaymentMethod = findViewById(R.id.tv_payment_method);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +34,39 @@ public class SettingActivity extends PageBarButton {
         //setContentView(R.layout.activity_setting);
         getSupportActionBar().hide();
         setAllBtnClickListener(SettingActivity.this);
+
+        View.OnClickListener onClickEditProfileListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        View.OnClickListener onClickChangePasswordListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, EditPasswordActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        View.OnClickListener onClickAddPaymentListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, AddPaymentActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        btnEditProfile.setOnClickListener(onClickEditProfileListener);
+        tvEditProfile.setOnClickListener(onClickEditProfileListener);
+        btnChangePass.setOnClickListener(onClickChangePasswordListener);
+        tvChangePass.setOnClickListener(onClickChangePasswordListener);
+        btnPaymentMethod.setOnClickListener(onClickAddPaymentListener);
+        tvPaymentMethod.setOnClickListener(onClickAddPaymentListener);
+
+
+
     }
 }

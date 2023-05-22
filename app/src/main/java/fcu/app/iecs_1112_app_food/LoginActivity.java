782e283aeaeab079396ee947ed2854a4bf32eead
JerryLIU <jerry2003.0401@gmail.com>
2023-05-22 11:47:10 +0800

    database base complete

--- app/src/main/java/fcu/app/iecs_1112_app_food/LoginActivity.java
+++ app/src/main/java/fcu/app/iecs_1112_app_food/LoginActivity.java
@@ -6,20 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button register_btn;
     private Button login_btn;
+    private AccountDatabaseHandler databaseHandler;
+    private EditText et_account;
+    private EditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-        register_btn = findViewById(R.id.register_btn_loginpage);
-        login_btn = findViewById(R.id.login_btn_loginpage);
-
-
+        setValueById();
+        databaseHandler = new AccountDatabaseHandler(this);
+        databaseHandler.open();
         View.OnClickListener onClickRegisterBtnListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,12 +35,26 @@ public class LoginActivity extends AppCompatActivity {
         View.OnClickListener onClickLoginBtnListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainPageActivity.class);
-                startActivity(intent);
+                if (databaseHandler.loginAccountSearch(et_account.getText().toString(), et_password.getText().toString(), LoginActivity.this)) {
+                    Intent intent = new Intent(LoginActivity.this, MainPageActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Wrong account or password", Toast.LENGTH_LONG).show();
+                }
             }
         };
         register_btn.setOnClickListener(onClickRegisterBtnListener);
         login_btn.setOnClickListener(onClickLoginBtnListener);
+
+
+    }
+
+    public void setValueById() {
+        register_btn = findViewById(R.id.register_btn_loginpage);
+        login_btn = findViewById(R.id.login_btn_loginpage);
+        et_account = findViewById(R.id.et_account_loginpage);
+        et_password = findViewById(R.id.et_password_loginpage);
     }
 
 

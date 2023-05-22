66cab68dd743ec4edea1ea9b119eb232dae01761
nguyenthanhphuoc <thanhphuoc1306vn@gmail.com>
2023-05-22 11:58:17 +0800

    done setting activity

--- app/src/main/java/fcu/app/iecs_1112_app_food/MyOrderActivity.java
+++ app/src/main/java/fcu/app/iecs_1112_app_food/MyOrderActivity.java
@@ -6,18 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
-public class MyOrderActivity extends PageBarButton {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyOrderActivity extends AppCompatActivity {
 
     private ImageButton imgBtnSettings;
 
+    private ListView lvFoods;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setLayoutId(R.layout.activity_my_order);
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_my_order);
-        getSupportActionBar().hide();
-        setAllBtnClickListener(MyOrderActivity.this);
-    }
+        setContentView(R.layout.activity_my_order);
 
+        imgBtnSettings = findViewById(R.id.img_btn_settings);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyOrderActivity.this, SettingActivity.class);
+                startActivity(intent);
+
+                lvFoods = findViewById(R.id.lv_foods);
+
+                List<FoodItem> foods = new ArrayList<FoodItem>();
+                foods.add(new FoodItem(R.drawable.fried_chicken, "fried chicken", 50));
+                foods.add(new FoodItem(R.drawable.hamburger, "hamburger", 23));
+                foods.add(new FoodItem(R.drawable.orange, "orange", 64));
+                foods.add(new FoodItem(R.drawable.spaghetti, "spaghetti", 67));
+                foods.add(new FoodItem(R.drawable.strawberry, "strawberry", 46));
+
+                ListViewAdapter adapter = new ListViewAdapter(MyOrderActivity.this, foods);
+                lvFoods.setAdapter(adapter);
+
+            }
+        };
+        imgBtnSettings.setOnClickListener(onClickListener);
+    }
 }
